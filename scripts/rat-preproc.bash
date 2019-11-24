405bad38b1e9bf29e40d9855a19431e8ec5cb14b
Chris Rafiaa <50058792+chrisrafiaa@users.noreply.github.com>
2019-11-24 01:29:50 -0500

    Update rat-preproc.bash

--- scripts/rat-preproc.bash
+++ scripts/rat-preproc.bash
@@ -3,15 +3,16 @@
 #loading module necessary
 module load minc-toolkit minc-toolkit-extras ANTs qbatch
 
+#making directory for unprocessed mincs
+mkdir unprocessed_minc
+
+mv ./*.mnc ./unprocessed_minc
+
 MINC_directory="/data/near/chris/studies/cfowler_data/MINC/MINC_t4/WT*"
 
 cd ${MINC_directory}
 
 mkdir preprocessed_minc
 
-for file in ./*mnc; do echo bash rat-preprocessing-v3.sh $file ./preprocessed_minc/$(basename $file .mnc)_preproc.mnc
+for file in ./*mnc; do echo bash rat-preprocessing-v3.sh ./unprocessed_minc/$file ./preprocessed_minc/$(basename $file .mnc)_preproc.mnc
 done | qbatch --ppj 4 -N rat-preproc -
-
-#mkdir unprocessed_minc
-
-#mv ./*.mnc ./unprocessed_minc
